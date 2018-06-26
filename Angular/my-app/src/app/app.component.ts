27b2c9f630d826f601c3c292c79f9918d0b319ed
SeasStarsRoses <pheide@t-online.de>
2018-06-26 21:21:24 +0100

    Added prime numbers

--- Angular/my-app/src/app/app.component.ts
+++ Angular/my-app/src/app/app.component.ts
@@ -7,9 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
-  public hello  = 'Hello';
-  currenttime : Date = new Date();
-  public reloadPage(){
+  hello = 'Hello';
+  primeNumbers = '';
+  maxLength = 8;
+  currenttime: Date = new Date();
+  public reloadPage() {
     location.reload(false);
+  }
+  public isPrimeNumber(candidate: number): boolean {
+    for (let i = 2; i < candidate; i++) {
+      if (candidate % i === 0) {
+        return false;
+      }
+    } return true;
+  }
+  public calculatePrimeNumbers() {
+    this.primeNumbers = '';
+    for (let i = 1; i <= 100; i++) {
+      const result = this.isPrimeNumber(i);
+      if (i !== 1 && result === true) {
+        this.primeNumbers = this.primeNumbers + i + ' ';
+        console.log(i);
+      }
+    }
   }
 }
